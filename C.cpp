f4930bcb020d6d7e2703691fd1cedf9824dd972e
pc3407 <pc3407@naver.com>
2020-04-23 17:45:50 +0900

    pointer 1/4

--- C.cpp
+++ C.cpp
@@ -4,6 +4,13 @@
 
 int main ()
 {
-
+	int i;
+	int j = 2;
+	i = j;	// assignment operator
+	int* ip;
+	ip = &j; // & : address-of operator
+	printf ( "%i, %p, %p\r\n",i, &j, ip);
+	printf ( "%i, %i\r\n",j,*ip ); // dereference operator(constent-of operaotr)
+	printf ( "%i, %p, %p\r\n",*ip,ip,&ip );
 	return 0;
 }
