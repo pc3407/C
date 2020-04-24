36e917d0daa236684d58a5b67821382d2bb7cbc0
pc3407 <pc3407@naver.com>
2020-04-24 18:54:34 +0900

    Array

--- C.cpp
+++ C.cpp
@@ -1,16 +1,22 @@
 ﻿// C.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 #include <stdio.h>
+#include <string.h>
+
+typedef unsigned char byte;
+typedef char STRING[20];
+
+void Test ( char a[] )
+{
+	printf ( "%s\r\n",a );
+}
 
 int main ()
 {
-	int i;
-	int j = 2;
-	i = j;	// assignment operator
-	int* ip;
-	ip = &j; // & : address-of operator
-	printf ( "%i, %p, %p\r\n",i, &j, ip);
-	printf ( "%i, %i\r\n",j,*ip ); // dereference operator(constent-of operaotr)
-	printf ( "%i, %p, %p\r\n",*ip,ip,&ip );
+	//char a[6] = {'h','e','l','l','o', 0};
+	STRING a = "hello";
+	//strcpy_s ( a,"world");
+	Test ( a );
+
 	return 0;
 }
